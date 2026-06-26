35470533941334aa994525e12bdadf52b347e124
jackmtlee <jackmtlee@126.com>
2026-06-25 21:41:37 -0400

    Lesson 1: review on basic data types

--- src/ca/qc/vc/week1/lesson1/BasicDataTypes.java
+++ src/ca/qc/vc/week1/lesson1/BasicDataTypes.java
@@ -2,5 +2,25 @@ package ca.qc.vc.week1.lesson1;
 
 public class BasicDataTypes {
 
+    public static void main(String[] args) {
+        byte smallNumber = 100;
+        short shortNumber = 30000;
+        int age = 25;
+        long population = 8_000_000_000L;
 
+        float price = 19.99f;
+        double distance = 12345.6789;
+
+        char grade = 'A';
+        boolean isJavaFun = true;
+
+        System.out.println("byte: " + smallNumber);
+        System.out.println("short: " + shortNumber);
+        System.out.println("int: " + age);
+        System.out.println("long: " + population);
+        System.out.println("float: " + price);
+        System.out.println("double: " + distance);
+        System.out.println("char: " + grade);
+        System.out.println("boolean: " + isJavaFun);
+    }
 }
