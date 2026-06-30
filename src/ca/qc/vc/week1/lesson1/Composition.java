31ad3f262a8e6488b6842e0d490b9f3dbc9a0e83
jackmtlee <jackmtlee@126.com>
2026-06-30 16:57:13 -0400

    Lesson 1: Composition

---
+++ src/ca/qc/vc/week1/lesson1/Composition.java
@@ -0,0 +1,40 @@
+package ca.qc.vc.week1.lesson1;
+
+
+class Engine {
+    public void start() {}
+    public void accelerate() {}
+    public void stop() {}
+}
+
+class Wheel {
+    public void inflate(int n) {}
+}
+
+class Window {
+    public void rollup() {}
+    public void rooldown() {}
+}
+
+class Door {
+    public Window window = new Window();
+
+    public void open() {}
+    public void close() {}
+}
+
+class Car {
+    public Engine engine = new Engine();
+    public Wheel[] wheel = new Wheel[4];
+    public Door left = new Door();
+    public Door right = new Door();
+
+    public Car() {
+        for (int i = 0; i < 4; ++i) {
+            wheel[i] = new Wheel();
+        }
+    }
+}
+
+public class Composition {
+}
