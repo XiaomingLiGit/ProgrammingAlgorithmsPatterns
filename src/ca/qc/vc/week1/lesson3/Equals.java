3491d0d6a1cf763adfd6287e45317ee2db74dfaf
jackmtlee <jackmtlee@126.com>
2026-07-01 15:36:55 -0400

    Lesson 2: methods in class Object

---
+++ src/ca/qc/vc/week1/lesson3/Equals.java
@@ -0,0 +1,37 @@
+package ca.qc.vc.week1.lesson3;
+
+public class Equals {
+    public static void main(String[] args) {
+        Book b1 = new Book("Java Basics");
+        Book b2 = new Book("Java Basics");
+        Book b3 = new Book("Advanced Java");
+
+        System.out.println(b1.equals(b2)); // true
+        System.out.println(b1.equals(b3)); // false
+        System.out.println(b1 == b2);      // false (different references)
+    }
+}
+
+
+//class Book {
+//    String title;
+//
+//    Book(String title) {
+//        this.title = title;
+//    }
+//
+//    @Override
+//    public boolean equals(Object obj) {
+//        if (this == obj) {
+//            return true;  // Same object
+//        }
+//
+//        if (obj == null || getClass() != obj.getClass()) {
+//            return false;
+//        }
+//
+//        Book other = (Book) obj;
+//
+//        return title.equals(other.title);  // Logical comparison
+//    }
+//}
---
+++ src/ca/qc/vc/week1/lesson3/HashCode.java
@@ -0,0 +1,62 @@
+package ca.qc.vc.week1.lesson3;
+
+
+import java.util.HashSet;
+
+class Book {
+    String title;
+    String author;
+
+    Book(String title) {
+        this.title = title;
+    }
+
+    Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+
+        Book other = (Book) obj;
+        return title.equals(other.title);
+    }
+
+    @Override
+    public int hashCode() {
+        return title.hashCode(); // use String's hashCode
+    }
+
+    @Override
+    public String toString() {
+        return "Book[title=\"" + title + "\", author=\"" + author + "\"]";
+    }
+}
+
+public class HashCode {
+
+    public static void main(String[] args) {
+        Book b1 = new Book("Java");
+        Book b2 = new Book("Java");
+
+        System.out.println(b1.equals(b2));   // true
+        System.out.println(b1.hashCode());   // same hash code
+        System.out.println(b2.hashCode());
+
+        HashSet<Book> set = new HashSet<>();
+
+        set.add(b1);
+        set.add(b2); // will not add a duplicate because equals() and hashCode() are aligned
+
+        System.out.println(set.size()); // 1
+    }
+}
+
