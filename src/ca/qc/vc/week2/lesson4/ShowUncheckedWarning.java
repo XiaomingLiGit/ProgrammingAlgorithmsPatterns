40a32244137845fc13830c9baf5f693c3d996b2b
jackmtlee <jackmtlee@126.com>
2026-07-03 12:59:10 -0400

    Lesson 4: Generic (motivating example)

---
+++ src/ca/qc/vc/week2/lesson4/ShowUncheckedWarning.java
@@ -0,0 +1,9 @@
+package ca.qc.vc.week2.lesson4;
+
+import java.util.ArrayList;
+public class ShowUncheckedWarning {
+    public static void main(String[] args) {
+        ArrayList list = new ArrayList();
+        list.add("Java Programming");
+    }
+}
