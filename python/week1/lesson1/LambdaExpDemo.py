261dbecefac7e4135ee5f126ba13c2b4bf147ef3
jackmtlee <jackmtlee@126.com>
2026-07-04 09:38:17 -0400

    Lesson 1: Lambda expression using Python

---
+++ python/week1/lesson1/LambdaExpDemo.py
@@ -0,0 +1,7 @@
+###################### Java Version #########################
+## Function<Integer, Integer> square = x -> x * x;
+## System.out.println(square.apply(5)); // 25
+#############################################################
+
+square = lambda x: x * x
+print(square(5))  # 25
