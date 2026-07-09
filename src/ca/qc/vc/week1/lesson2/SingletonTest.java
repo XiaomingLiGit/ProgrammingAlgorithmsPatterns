1488b994d3f5032d5336426685c9f9e51975b13f
jackmtlee <jackmtlee@126.com>
2026-07-09 13:51:32 -0400

    Lesson 3: demo of singleton

--- src/ca/qc/vc/week1/lesson2/SingletonTest.java
+++ src/ca/qc/vc/week1/lesson2/SingletonTest.java
@@ -1,5 +1,5 @@
 package ca.qc.vc.week1.lesson2;
-
+//import java.math.BigInteger
 /**
  * this code snippet shows a simple version of Singleton
  * lazy mode and thread-unsafe
@@ -41,6 +41,15 @@ public class SingletonTest {
         // Usage:
         Singleton s1 = Singleton.getInstance();
         Singleton s2 = Singleton.getInstance();
+
+        System.out.println("s1 reference is " + s1);
+        System.out.println("s2 reference is " + s2);
+
+        if  (s1 == s2) {System.out.println("the same instance");}
+        else {System.out.println("different instances" );}
+
+        System.out.println("s1 hash code is " + s1.hashCode());
+        System.out.println("s2 hash code is " + s2.hashCode());
 
         // s1 and s2 refer to the same instance.
     }
