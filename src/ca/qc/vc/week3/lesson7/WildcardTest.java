1d5a0f0db4287f7aa829dab854ee2a3b141f31bc
jackmtlee <jackmtlee@126.com>
2026-07-10 11:49:20 -0400

    Lesson 7: wildcards Demo

---
+++ src/ca/qc/vc/week3/lesson7/WildcardTest.java
@@ -0,0 +1,44 @@
+package ca.qc.vc.week3.lesson7;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Employee {
+    private String name;
+
+    public String getName() {
+        return this.name;
+    }
+}
+
+class Manager extends Employee {
+}
+
+class Intern extends Employee {
+}
+
+public class WildcardTest {
+    // producer
+    public static void printNames(List<? extends Employee> employees) {
+        for (Employee e : employees) {
+            System.out.println(e.getName());
+        }
+    }
+    // consumer
+    public static void addEmployees(List<? super Employee> list) {
+        list.add(new Manager());
+        list.add(new Intern());
+    }
+
+    public static void main(String[] args) {
+        List<Employee> employeeList = new ArrayList<>();
+        List<Manager> managerList = new ArrayList<>();
+        List<Intern> internList = new ArrayList<>();
+
+        addEmployees(employeeList);
+
+        printNames(employeeList);
+        printNames(managerList);
+        printNames(internList);
+    }
+}
