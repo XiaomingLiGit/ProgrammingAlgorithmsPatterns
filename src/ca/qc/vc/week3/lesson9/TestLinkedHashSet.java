1cd81e5506cb15418f36ff4e70ab63eef76f0f6b
jackmtlee <jackmtlee@126.com>
2026-07-11 14:37:06 -0400

    Lesson 9: Set

---
+++ src/ca/qc/vc/week3/lesson9/TestLinkedHashSet.java
@@ -0,0 +1,27 @@
+package ca.qc.vc.week3.lesson9;
+
+import java.util.*;
+
+public class TestLinkedHashSet {
+
+    public static void main(String[] args) {
+
+        // Create a linked hash set
+        Set<String> set = new LinkedHashSet<>();
+
+        // Add strings to the set
+        set.add("London");
+        set.add("Paris");
+        set.add("New York");
+        set.add("San Francisco");
+        set.add("Beijing");
+        set.add("New York");
+
+        System.out.println(set);
+
+        // Display the elements in the linked hash set
+        for (String element : set) {
+            System.out.print(element.toLowerCase() + " ");
+        }
+    }
+}
