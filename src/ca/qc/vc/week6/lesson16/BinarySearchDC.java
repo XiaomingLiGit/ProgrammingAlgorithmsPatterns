4efbe37672b7ac0bf3eb388efa3bd57824293284
jackmtlee <jackmtlee@126.com>
2026-08-10 07:50:19 -0400

    Lesson 17: divide-and-conquer

---
+++ src/ca/qc/vc/week6/lesson16/BinarySearchDC.java
@@ -0,0 +1,31 @@
+package ca.qc.vc.week6.lesson16;
+
+public class BinarySearchDC {
+
+    public static int search(int[] nums, int target) {
+
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target)
+                return mid;
+
+            if (nums[mid] < target)
+                left = mid + 1;
+            else
+                right = mid - 1;
+        }
+
+        return -1;
+    }
+
+
+    public static void main(String[] args) {
+
+
+    }
+}
